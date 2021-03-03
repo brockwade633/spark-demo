7498c309bbc943bb87c1699a6ca2081cad78d2c5
Brock Wade <bwade@Brocks-MacBook.local>
2021-03-02 20:26:25 -0800

    add nested stack

--- local_deploy.sh
+++ local_deploy.sh
@@ -3,8 +3,10 @@
 stackName=${1:-spark-demo}
 region=${2:-us-west-2}
 
+echo 'cfn-lint ...'
 cfn-lint cfn/spark-demo.yaml
 
+echo 'deploying...'
 aws cloudformation deploy --template-file cfn/spark-demo.yaml \
     --stack-name $stackName \
     --capabilities CAPABILITY_IAM \
