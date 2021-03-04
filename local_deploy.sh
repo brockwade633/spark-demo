ec08369563d8fcfb45e00f8c065b5e041080c69f
Brock Wade <bwade@Brocks-MacBook.local>
2021-03-03 20:11:41 -0800

    basic cfn

--- local_deploy.sh
+++ local_deploy.sh
@@ -2,12 +2,23 @@
 
 stackName=${1:-spark-demo}
 region=${2:-us-west-2}
+bucket=${3:-deployment-cfn-templates}
 
 echo 'cfn-lint ...'
 cfn-lint cfn/spark-demo.yaml
 
+echo 'packaging...'
+aws cloudformation package \
+    --template-file cfn/${stackName}.yaml \
+    --s3-bucket ${bucket} \
+    --s3-prefix ${stackName} \
+    --region ${region} \
+    --output-template-file cfn/${stackName}-packaged.yaml
+
 echo 'deploying...'
-aws cloudformation deploy --template-file cfn/spark-demo.yaml \
-    --stack-name $stackName \
-    --capabilities CAPABILITY_IAM \
-    --region $region
+aws cloudformation deploy \
+    --no-fail-on-empty-changeset \
+    --template-file cfn/${stackName}-packaged.yaml \
+    --stack-name ${stackName} \
+    --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND \
+    --region ${region}
